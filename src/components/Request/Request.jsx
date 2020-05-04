716eec292d94af70b71482c37e0bcc34b4699a49
Anastasia Morozova <morozova.asia@gmail.com>
2020-05-03 22:27:23 -0700

    refactoring

--- src/components/Request/Request.jsx
+++ src/components/Request/Request.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Request.css';
 
+const RadioButtonGroup = ({ children }) => {
+  return (
+    <>
+      { children }
+    </>
+  );
+};
+
 const RadioButton = ({ name, value, onChange }) => (
   <>
     <input id={value} type="radio" name={name} value={value} onChange={onChange} />
@@ -13,25 +21,17 @@ const Request = ({ url, onUrlChange, body, onBodyChange, onMethodChange, onSubmi
   <form className={styles.Request} nonSubmit={onSubmit} >
     <input type="text" value={url} onChange={onUrlChange} />
 
-    <RadioButton name="method" value ="POST" onChange={onMethodChange} />
-    <input id="POST" type="radio" name="method" value="POST" onChange={onMethodChange} />
-    <label htmlFor="POST"> POST</label>
+    
 
-    <RadioButton name="method" value ="GET" onChange={onMethodChange} />
-    <input id="GET" type="radio" name="method" value="GET" onChange={onMethodChange} />
-    <label htmlFor="GET">GET</label>
+    <RadioButtonGroup name="method" onChange={onMethodChange}>
+      <RadioButton value="POST" />
+      <RadioButton value ="GET"  />
+      <RadioButton  value ="PATCH"  />
+      <RadioButton  value ="PUT" />
+      <RadioButton  value ="DELETE" />
 
-    <RadioButton name="method" value ="PATCH" onChange={onMethodChange} />
-    <input id="PATCH" type="radio" name="method" value="PATCH" onChange={onMethodChange} />    
-    <label htmlFor="PATCH">PATCH</label>
-
-    <RadioButton name="method" value ="PUT" onChange={onMethodChange} />
-    <input id="PUT" type="radio" name="method" value="PUT" onChange={onMethodChange} />
-    <label htmlFor="PUT">PUT</label>
-
-    <RadioButton name="method" value ="DELETE" onChange={onMethodChange} />
-    <input id="DELETE" type="radio" name="method" value="DELETE" onChange={onMethodChange} />
-    <label htmlFor="DELETE">DELETE</label>
+    </RadioButtonGroup>
+    
 
     <textarea value ={body} onChange={onBodyChange}></textarea>
     <button>Submit</button>
