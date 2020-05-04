99192b8971a3e521a0f3eb5da9bf27305c707f16
Anastasia Morozova <morozova.asia@gmail.com>
2020-05-03 21:15:02 -0700

    running onto a wierd error

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
 export default function App() {
-  return <Request />;
+  return <Request/>;
 }
   
--- src/components/Request/Request.jsx
+++ src/components/Request/Request.jsx
@@ -1,42 +1,42 @@
 import React from 'react';
-omport PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-const Request = ({url, onUrlChange, body, onBodyChange, onMethodChange, onSubmit }) => (
-    <form nonSubmit={onSubmit} >
-        <input type="text" value={url} onChange={onUrlChange} />
-        <label>
+const Request = ({ url, onUrlChange, body, onBodyChange, onMethodChange, onSubmit }) => (
+  <form nonSubmit={onSubmit} >
+    <input type="text" value={url} onChange={onUrlChange} />
+    <label>
             POST:
-            <input type="radio" name="method" value="POST" onChange={onMethodChange} />
-        </label>
-        <label>
+      <input type="radio" name="method" value="POST" onChange={onMethodChange} />
+    </label>
+    <label>
             GET:
-            <input type="radio" name="method" value="GET" onChange={onMethodChange} />
-        </label>
-        <label>
+      <input type="radio" name="method" value="GET" onChange={onMethodChange} />
+    </label>
+    <label>
             PATCH:
-            <input type="radio" name="method" value="PATCH" onChange={onMethodChange} />
-        </label>
-        <label>
+      <input type="radio" name="method" value="PATCH" onChange={onMethodChange} />
+    </label>
+    <label>
             PUT:
-            <input type="radio" name="method" value="PUT" onChange={onMethodChange} />
-        </label>
-        <label>
+      <input type="radio" name="method" value="PUT" onChange={onMethodChange} />
+    </label>
+    <label>
             DELETE:
-            <input type="radio" name="method" value="DELETE" onChange={onMethodChange} />
-        </label>
-        <textarea value ={body} onChange={onBodyChange}></textarea>
-        <button>Submit</button>
-    </form>
-)
+      <input type="radio" name="method" value="DELETE" onChange={onMethodChange} />
+    </label>
+    <textarea value ={body} onChange={onBodyChange}></textarea>
+    <button>Submit</button>
+  </form>
+);
 
 Request.propTypes = {
-    url: PropTypes.string.isRequired,
-    method: PropTypes.string.isRequired,
-    body: PropTypes.object.isRequired,
-    onUrlChange: PropTypes.func.isRequired,
-    onMethodChange: PropTypes.func.isRequired, 
-    onBodyChange: PropTypes.func.isRequired,
-    onSubmit: PropTypes.func.isRequired
-}
+  url: PropTypes.string.isRequired,
+  method: PropTypes.string.isRequired,
+  body: PropTypes.object.isRequired,
+  onUrlChange: PropTypes.func.isRequired,
+  onMethodChange: PropTypes.func.isRequired, 
+  onBodyChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired
+};
 
 export default Request; 
