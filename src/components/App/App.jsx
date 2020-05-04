bd395d4447715d4fa4f1f148a7bc7700a58cb5b4
Anastasia Morozova <morozova.asia@gmail.com>
2020-05-03 22:12:24 -0700

    progress

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Request from '../Request/Request';
+
 
 export default function App() {
   return <Request/>;
--- src/components/Request/Request.jsx
+++ src/components/Request/Request.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Request.css';
+
+const RadioButton = ({ name, value, onChange }) => (
+  <>
+    <input id={value} type="radio" name={name} value={value} onChange={onChange} />
+    <label htmlFor={value}>{value}</label>
+  </>
+);
 
 const Request = ({ url, onUrlChange, body, onBodyChange, onMethodChange, onSubmit }) => (
-  <form nonSubmit={onSubmit} >
+  <form className={styles.Request} nonSubmit={onSubmit} >
     <input type="text" value={url} onChange={onUrlChange} />
-    <label>
-            POST:
-      <input type="radio" name="method" value="POST" onChange={onMethodChange} />
-    </label>
-    <label>
-            GET:
-      <input type="radio" name="method" value="GET" onChange={onMethodChange} />
-    </label>
-    <label>
-            PATCH:
-      <input type="radio" name="method" value="PATCH" onChange={onMethodChange} />
-    </label>
-    <label>
-            PUT:
-      <input type="radio" name="method" value="PUT" onChange={onMethodChange} />
-    </label>
-    <label>
-            DELETE:
-      <input type="radio" name="method" value="DELETE" onChange={onMethodChange} />
-    </label>
+
+    <RadioButton name="method" value ="POST" onChange={onMethodChange} />
+    <input id="POST" type="radio" name="method" value="POST" onChange={onMethodChange} />
+    <label htmlFor="POST"> POST</label>
+
+    <RadioButton name="method" value ="GET" onChange={onMethodChange} />
+    <input id="GET" type="radio" name="method" value="GET" onChange={onMethodChange} />
+    <label htmlFor="GET">GET</label>
+
+    <RadioButton name="method" value ="PATCH" onChange={onMethodChange} />
+    <input id="PATCH" type="radio" name="method" value="PATCH" onChange={onMethodChange} />    
+    <label htmlFor="PATCH">PATCH</label>
+
+    <RadioButton name="method" value ="PUT" onChange={onMethodChange} />
+    <input id="PUT" type="radio" name="method" value="PUT" onChange={onMethodChange} />
+    <label htmlFor="PUT">PUT</label>
+
+    <RadioButton name="method" value ="DELETE" onChange={onMethodChange} />
+    <input id="DELETE" type="radio" name="method" value="DELETE" onChange={onMethodChange} />
+    <label htmlFor="DELETE">DELETE</label>
+
     <textarea value ={body} onChange={onBodyChange}></textarea>
     <button>Submit</button>
   </form>
