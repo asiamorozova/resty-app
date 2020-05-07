6d4959d9528a03e558220e30c6f1f5bc2491f661
Anastasia Morozova <morozova.asia@gmail.com>
2020-05-07 15:42:18 -0700

    node

--- src/components/Request/Header/Header.test.jsx
+++ src/components/Request/Header/Header.test.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
+import Header from './Header';
 
 describe('App component', () => {
-  it('renders App', () => {
-    const wrapper = shallow(<App />);
+  it('renders Header and Resty', () => {
+    const wrapper = shallow(
+      <Header />);
     expect(wrapper).toMatchSnapshot();
   });
 });
-  
