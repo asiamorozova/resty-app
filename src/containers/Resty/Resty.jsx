1f179ff1f145032443e090e671c59711bc7430c2
Anastasia Morozova <morozova.asia@gmail.com>
2020-05-14 13:43:43 -0700

    resy 1

--- src/containers/Resty/Resty.jsx
+++ src/containers/Resty/Resty.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import Request from '../../components/Request/Request';
-
+import { fetchRequest } from '../../fetchRequest';
+import Response from '../../components/Response/Response';
+import HistoryList from '../../components/Response/Response';
 
 const Resty = () => {
   const [method, setMethod] = useState('');
   const [url, setUrl] = useState('');
   const [textBody, setTextBody] = useState('');
+  const [response, setResponse] = useState([]);
+  const [history, setHistory] = useState([]);
 
   const handleChange = ({ target }) => {
     if(target.name === 'method') setMethod(target.value);
@@ -15,18 +19,25 @@ const Resty = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    
+    fetchRequest(url, method, textBody)
+      .then(json => {
+        setResponse(json);
+        setHistory(preHistory => [{ url, method }, ...preHistory]);
+      }); 
   };
 
   return (
-    <Request
-      url={url} 
-      textBody={textBody} 
-      onChange={handleChange}
-      onSubmit={handleSubmit}
-    />
+    <>
+      <HistoryList history={history} />
+      <Form
+        url={url} 
+        textBody={textBody} 
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+      <Response response={response} />
+    </>
   );
-
 };
 
 export default Resty;
